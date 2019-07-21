ce44319a2a6b6b0d7ae1c4509c93de040fd9bacb
Soham Bhattacharjee <soham.bhattacharjee@oracle.com>
2019-07-21 14:22:50 +0530

    Initial Commit

---
+++ SBCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SBCoreData
+//
+//  Created by Soham Bhattacharjee on 21/07/19.
+//  Copyright © 2019 Soham Bhattacharjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
