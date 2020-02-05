f906e24b10fabce31c8ed83a430410ec4d72b801
Soham Bhattacharjee <sohamb.1390@gmail.com>
2020-02-05 13:11:58 +0530

    - iOS version changed

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SBCoreData",
-    platforms: [.iOS(SupportedPlatform.IOSVersion.v11)],
+    platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
